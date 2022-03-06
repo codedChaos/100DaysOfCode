36f5de5f91e5aef105b3bc864d06d1f47749bd4b
William "codedChaos" Smyth <devopsmyth@gmail.com>
2022-03-06 06:12:54 -0500

    refactor(first-commit): refactoring first commit to repo
    add first commit to begin tracking changes

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # 100DaysOfCode
-#100DaysOfCode-Challenge-Repo
+
+## Challenge-Log
+
+[#100DaysOfCode Official Website Link][1]
+
+<style type="text/css">
+    h5  {
+        font-family:    Ubuntu, "Helvetica Neue", Helvetica, sans-serif;
+        font-size:      18px;
+        font-weight:    normal;
+    }  
+    h2 {
+        font-family:    Georgia, Verdana, monospace;
+        font-size:      24px;
+        font-weight:    medium;
+        font-style:     bold;
+        color:          orange;
+        text-align:     left;
+        padding:        0px 0px 0px 10px;
+    }
+    h1 {
+        font-size:      48px;
+        text-align:     center;
+        border:         5px solid;
+        color:          chartreuse;
+        padding:        10px 10px 10px 10px;
+        border-radius:  25%;
+    }
+    href {
+        color:          red;
+    }
+</style>
+
+----------------------------------------------------------------
+
+### Table of Contents
+
+- [100DaysOfCode](#100daysofcode)
+  - [Challenge-Log](#challenge-log)
+    - [Table of Contents](#table-of-contents)
+    - [Daily Entries](#daily-entries)
+      - [Day1](#day1)
+        - [Example](#example)
+        - [My Solution](#my-solution)
+    - [Links & References](#links--references)
+
+&nbsp;
+
+### Daily Entries
+
+#### [Day1][3]
+
+ Working on helping other user solve coding challenge question with JavaScript. Question Specifics: remove duplicate entries from an array of integers, leaving behind empty elements that have been sorted to the end of the array.
+
+##### [Example][3]
+
+```javascript
+int[] nums = [1, 1, 2, 2, 2, 3, 3, 3];
+// write function that returns the number of unique integers in the array along with
+// a sorted array that has moved all empty elements to the end of the array.
+var result = function(nums) {
+    // remove duplicate entries and resort the blanks to the end of the array
+    return (countOfUniqueIntegers, nums);
+}
+
+console.log(result(nums)); 
+
+// expected output below
+
+[3, [1, 2, 3, '', '', '', '', '']]
+// array is changed in-place to keep memory usage constant O(1). This array length
+// is the same as original array length. You don't have to empty the array elements
+// but it would need to look like this or similar.
+
+[3, [1, 2, 3, 1, 2, 2, 3, 3]]; 
+// as long as the first consecutive elements are in unique and ascending order.
+
+```
+
+##### [My Solution][3]
+
+```javascript
+    // paste code from working solution
+```
+
+&nbsp;
+
+### Links & References
+
+[1]: https://100daysofcode.com/
+[2]: https://github.com/codedChaos/100DaysOfCode/wiki/Day1
+[3]: https://github.com/codedChaos/100DaysOfCode
---
+++ helping-other-user_array_strip_unique.js
@@ -0,0 +1,127 @@
+/* Given an integer array nums sorted in non-decreasing order, 
+reemove the duplicates in-place such that each unique
+element appears only once. The relative order of the elements 
+should be kept the same.
+
+Since it is impossible to change the length of the array in some
+languages, you must instead have the result be placed in 
+the first part of the array nums. More formally, if there are k
+elements after removing the duplicates, then the first k
+elements of nums should hold the final result. It does not matter
+what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of
+nums.
+
+Do not allocate extra space for another array. You must do this by
+modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code: */
+
+/* int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,]
+Explanation: Your function should return k = 2, with the first two elements of nums being 
+1 and 2 respectively. It does not matter what you leave beyond the returned k (hence they 
+are underscores). */
+//
+//
+
+const numarray = [1,1,2,2,3,4,5,6,7,7,8,8,9,10,11,11,11,12]; // test input array
+
+/*var sliceSwap = function(nums, start, end) {
+    // write function to shallow copy the array back into ascending order with NULL values
+    // appended to the end of the array
+    // call function from removeDupplicates() function whenever spaces are created() {
+    
+    //code goes here
+    return numarray.slice(0, start) + numarrnumarray.slice(start, end);
+},*/
+var moveArrayChunk = function(arrN, start, remove, addons) {
+    /*  write function to use copyWithin to do an in-memory copy of chunks of the array
+        to fill in gaps or blank array elements.
+    
+        code a badass function
+    
+        PARAMETERS: Usage and examples
+        param nums - original array of number
+        param start [optional] - zero-based index to start copying elements from.
+                                 if negative, start will count backwards from the 
+                                 end of the array.'
+                                 
+                                 **If start is ommitted, copy will start from index 0.**
+
+        param end [optional]   - zero-based index to end copying elements from. copyWithin
+                                 copies up to but not including the end. If negative, end
+                                 will be counted backwards from end of the array.
+    
+                                 **If end is ommitted, copy will end at last index of array.**
+    */
+    
+    // my code
+    return (nums.slice());
+};
+
+var removeDuplicates = function(nums) {
+  var count = 0;
+
+/*   for (var i = 0; i < nums.length; i++){
+    if (nums[i + 1] === nums[i]) nums[i] = "";
+  }
+
+  for(var x = 0; x < nums.length; x++){
+    if (nums[x] === ''){
+      for (j = x; j < nums.length - x - 1; j++){
+        temp = nums[j];
+        nums[j] = nums[j+1];
+        nums[j+1] = temp;
+      }
+    }
+  }
+
+  var y = 0;
+  while(nums[y] != ""){
+    count++;
+    y++;
+  } */
+
+  //my way of filtering O(1) memory footprint
+  var arrLen = nums.length;
+  for (var x = 0; x < arrLen; x++) {
+    if (x != '' && x != nums.lastIndexOf(nums[x])){
+        removeSteps = nums.lastIndexOf(nums[x]) - x;
+        while (removeSteps > 0) {
+            nums[x + removeSteps] = "";
+            removeSteps--;
+        }
+        moveArrayChunk(nums, x, nums.lastIndexOf(nums[x]))
+    }
+    
+  }
+  
+  console.log(count, nums);
+  return (count, nums);
+};
+
+console.log(numarray, "nums array");
+
+var response = removeDuplicates(numarray);
+
+console.log(response);
